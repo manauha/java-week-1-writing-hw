9f2642e1a6aff9d1a37959dc809108521813e0a7
manauha <manauha@gmail.com>
2023-10-20 09:42:03 +0100

    My homework week1

---
+++ src/homework_week_5/Programme_3_PrintInitials.java
@@ -0,0 +1,22 @@
+package homework_week_5;
+
+/**
+ * 3. Write a program that will print your initials to stand output in letters that
+ * nine lines tall. Each big letter should e made up of a buch of *'s. For
+ * example, if your initials were "JAJA" then the out would look something
+ * like:
+ */
+
+public class Programme_3_PrintInitials {
+    public static void main(String[] args){
+        System.out.println("*********  ********* ********* *********");
+        System.out.println("      **   **     **       **  **     **");
+        System.out.println("      **   **     **       **  **     **");
+        System.out.println("      **   **     **       **  **     **");
+        System.out.println("      **   *********       **  *********");
+        System.out.println("  **  **   **     **   **  **  **     **");
+        System.out.println("  **  **   **     **   **  **  **     **");
+        System.out.println("   ****    **     **    ****   **     **");
+        System.out.println("    **     **     **     **    **     **");
+    }
+}
